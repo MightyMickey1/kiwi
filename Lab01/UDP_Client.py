c378f0d70baaf5fca2e55deab4fefd22e79dc916
AJ Gregory <aj@whirlingdervii.com>
2025-09-25 20:08:38 -0700

    Updated to allow client messaging

--- Lab01/UDP_Client.py
+++ Lab01/UDP_Client.py
@@ -1,13 +1,22 @@
 import socket
 
-clientMsg = 'Good morning, Shiron'.encode('utf-8')
+# Setup socket link
+welcomeClientMsg = 'Good morning, Shiron'.encode('utf-8')
 serverAddr = ('10.40.121.124', 5268)
 bufferSize = 1024
-
 UDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDPClient.sendto(clientMsg, serverAddr)
+
+# Establish first contact
+UDPClient.sendto(welcomeClientMsg, serverAddr)
 serverMsg, addr = UDPClient.recvfrom(bufferSize)
 serverMsg = serverMsg.decode('utf-8')
+print('Welcome from server: ', serverMsg)
+print(f'Server Addess: {addr[0]}:{addr[1]}\n')
 
-print('Data from server: ', serverMsg)
-print(f'Server Addess: {addr[0]}:{addr[1]}')
+# User inputs messages to send to server
+while True:
+    clientMsg = input('Command to send to Server: ')
+    UDPClient.sendto(clientMsg, serverAddr)
+    serverMsg, addr = UDPClient.recvfrom(bufferSize)
+    serverMsg = serverMsg.decode('utf-8')
+    print('Message from server: ', serverMsg)
